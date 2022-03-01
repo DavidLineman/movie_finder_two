f2af32798f6c836f843a97fc5a9f2a1b2d154962
DavidLineman <dline215@gmail.com>
2022-03-01 09:58:18 -0500

    fixed issue with routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Home from './Home';
+import Movie from './Movie';
 
 import './App.css';
 
@@ -16,6 +17,7 @@ const App = () => {
       </nav>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/movie/:id" element={<Movie />} />
         <Route component={NotFound} />
       </Routes>
     </Router>
